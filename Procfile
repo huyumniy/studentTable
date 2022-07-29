78b6874ca5807d3deb0e3cb49e665bb99e6ab48f
huyumniy <110224080+huyumniy@users.noreply.github.com>
2022-07-29 12:31:28 +0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python studentTable.py
---
+++ bot_run.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call %~dp0venv\Scripts\activate
+
+set TOKEN=5385147452:AAEQHoHBmcQnsrQLy7Ikw1bDztuSAcq4T9w
+
+python studentTable.py
+
+pause
