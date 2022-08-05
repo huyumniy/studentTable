db31533f1f89c8cfbee3dc9185da03566c248bbd
huyumniy <110224080+huyumniy@users.noreply.github.com>
2022-08-05 17:53:04 +0300

    Update studentTable.py

--- studentTable.py
+++ studentTable.py
@@ -1,3 +1,4 @@
+import os
 import json
 import requests
 from os.path import exists
@@ -7,7 +8,6 @@ from selenium.webdriver.common.by import By
 from aiogram import Bot, Dispatcher, executor, types
 
 
-PATH = 'C:\Program Files (x86)\geckodriver.exe'
 bot = Bot(token="5385147452:AAEQHoHBmcQnsrQLy7Ikw1bDztuSAcq4T9w")
 dp = Dispatcher(bot)
 
@@ -78,11 +78,12 @@ if exists('data.json'):
                 if k.lower() == message.text.split('/')[1].lower():
                     await bot.send_message(message.chat.id, 'Зачекайте будь ласка, обробляю фото...')
                     table = 'http://193.189.127.179:5010/time-table/student?' + v
-                    option = webdriver.FirefoxOptions()
-                    option.add_argument("--headless")
+                    option = webdriver.ChromeOptions()
+                    option.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
                     option.add_argument("--disable-dev-shm-usage")
                     option.add_argument("--no-sandbox")
-                    browser = webdriver.Firefox(executable_path=r'C:\Program Files (x86)\geckodriver.exe', options=option)
+                    browser = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=option)
                     browser.get(table)
                     body = browser.find_element(By.ID, 'time-tablew6')
                     body.screenshot('picture.png')
